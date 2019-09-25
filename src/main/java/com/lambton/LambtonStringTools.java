1b7e87abd1bad888b9dfc5e63e0a173af6c0f62f
shiji703 <shijieugin3@gmail.com>
2019-09-25 19:09:45 -0400

    [shiji] reversed string "lambton"

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,4 +1,28 @@
 package com.lambton;
 
-public class LambtonStringTools {
-}
+public class LambtonStringTools
+{
+        //Reversing String
+        public static void main(String[] args)
+        {
+            String s = "Lambton";
+            char r[] = s.toCharArray();
+            char rev[] = new char[r.length];
+            int cnt = 0;
+            for (int i = r.length - 1; i >= 0; i--)
+            {
+                rev[cnt++] = r[i];
+            }
+            String reverseString = new String(rev);
+            System.out.println("Reverse String = " + reverseString);
+        }
+
+    }
+
+
+
+
+
+
+
+
